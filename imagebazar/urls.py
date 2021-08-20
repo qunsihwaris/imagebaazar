4898976a1cb7d095dcc29b0ee3d1fe9b032bc168
qunsihwaris <61161888+qunsihwaris@users.noreply.github.com>
2021-08-20 15:24:24 +0500

    Add files via upload

---
+++ imagebazar/urls.py
@@ -0,0 +1,33 @@
+"""imagebazar URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core.views import index, SignUp, SignIn, Posts
+from deshboard.views import deshboard, CreatePost, CreatedPosts, signout
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', SignUp, name='signup_page'),
+    path('signin/', SignIn, name='signin_page'),
+    path('welcome/', deshboard, name='welcome_page'),
+    path('upload/', CreatePost , name='upload'),
+    path('post/', CreatedPosts, name='ownposts'),
+    path('signout', signout, name='signout_page'),
+    path('', Posts , name='home_page')
+
+]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
